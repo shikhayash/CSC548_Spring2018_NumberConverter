eca787fc3c545d417481d188e24622b6bae86662
awesomefat <awesomefat@gmail.com>
2018-02-01 22:02:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_numberconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.awesomefat.csc548_spring2018_numberconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private TextView bitsTV;
+    private EditText mbET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
+        this.bitsTV = (TextView)this.findViewById(R.id.bitsTV);
+        this.mbET = (EditText)this.findViewById(R.id.mbET);
+    }
+
+    public void clickMeButtonPressed(View v)
+    {
+        String sValue = this.mbET.getText().toString();
+        int iValue = Integer.parseInt(sValue);
+        bitsTV.setText("" + (iValue + 5));
+
+    }
+}
