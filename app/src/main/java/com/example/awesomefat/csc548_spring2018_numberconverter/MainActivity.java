ef5994f509159447098314f8883c305000eb9e66
Shikha Desai <ssdesai.shikha@gmail.com>
2018-02-08 16:32:54 -0600

    Number conversion changes

--- app/src/main/java/com/example/awesomefat/csc548_spring2018_numberconverter/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_numberconverter/MainActivity.java
@@ -9,9 +9,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity
 {
     //made a change
-    
-    private TextView bitsTV;
-    private EditText mbET;
+
+    private TextView bit;
+    private TextView kbit;
+    private TextView mbit;
+    private TextView gbit;
+    private TextView tbit;
+   private TextView byteTV;
+    private TextView kbyte;
+
+    private TextView gbyte;
+    private TextView tbyte;
+
+
+    private EditText mbyte;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,15 +31,59 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
 
-        this.bitsTV = (TextView)this.findViewById(R.id.bitsTV);
-        this.mbET = (EditText)this.findViewById(R.id.mbET);
+        this.bit = (TextView)this.findViewById(R.id.bit);
+        this.kbit = (TextView)this.findViewById(R.id.kbit);
+        this.mbit = (TextView)this.findViewById(R.id.mbit);
+        this.gbit = (TextView)this.findViewById(R.id.gbit);
+        this.tbit = (TextView)this.findViewById(R.id.tbit);
+
+        this.byteTV = (TextView)this.findViewById(R.id.byteTV);
+        this.kbyte = (TextView)this.findViewById(R.id.kbyte);
+
+        this.gbyte = (TextView)this.findViewById(R.id.gbyte);
+        this.tbyte = (TextView)this.findViewById(R.id.tbyte);
+
+        this.mbyte = (EditText)this.findViewById(R.id.mbyte);
+    }
+
+    private String roundOffNumber(double num)
+    {
+        return String.format("%.6f", num);
     }
 
     public void clickMeButtonPressed(View v)
     {
-        String sValue = this.mbET.getText().toString();
-        int iValue = Integer.parseInt(sValue);
-        bitsTV.setText("" + (iValue + 5));
+        String sInputMB = this.mbyte.getText().toString();
+
+        int iInputMB = Integer.parseInt(sInputMB);
+
+        int mbits = iInputMB * 8;
+
+        int kbytes = iInputMB * 1024;
+        int kbits = kbytes * 8;
+
+        int bytes = kbytes * 1024;
+       int bits = bytes * 8;
+
+        double gbytes = (double)iInputMB / 1024;
+        double gbits = gbytes * 8;
+
+        double tbytes = (double)gbytes / 1024;
+        double tbits = tbytes * 8;
+
+
+        bit.setText("" +  bits );
+        kbit.setText("" +  kbits);
+        mbit.setText("" +  mbits);
+        gbit.setText("" +  roundOffNumber(gbits));
+        tbit.setText("" +  roundOffNumber(tbits));
+
+        byteTV.setText("" +  bytes);
+        kbyte.setText("" +  kbytes);
+        //mbyte.setText("" +  iInputMB + " MB");
+        gbyte.setText("" +  roundOffNumber(gbytes));
+        tbyte.setText("" +  roundOffNumber(tbytes));
 
     }
+
 }
